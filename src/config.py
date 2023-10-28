7e55c0d3460eaf172cc066d77633b60f924ee5a4
ronan-s1 <singpurwala13@gmail.com>
2023-10-29 00:42:17 +0100

    error check config ids

--- src/config.py
+++ src/config.py
@@ -9,5 +9,15 @@ def read_config():
 
     sellers = dict(config.items("Sellers"))
     buyers = dict(config.items("Buyers"))
-    
+
+    # Check for duplicate seller and buyer IDs
+    seller_ids = set(sellers.values())
+    buyer_ids = set(buyers.values())
+
+    if len(seller_ids) != len(sellers):
+        raise ValueError("Duplicate seller IDs found in config file.")
+
+    if len(buyer_ids) != len(buyers):
+        raise ValueError("Duplicate buyer IDs found in config file.")
+
     return host, port, sellers, buyers
